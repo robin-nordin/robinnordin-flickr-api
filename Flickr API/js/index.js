d38c71010f7580e2a9bd45cc480a620d51d70727
Robin Nordin <61803782+robin-nordin@users.noreply.github.com>
2023-02-15 15:34:15 +0100

    .

--- Flickr API/js/index.js
+++ Flickr API/js/index.js
@@ -6,77 +6,65 @@ let url = '${baseUrl}?api_key=${apiKey}&method=${method}&text=${text}&format=jso
 
 */
 
-document.querySelector('button').addEventListener('click', async () => {
+document.querySelector("button").addEventListener("click", async () => {
+  let text = document.querySelector("#search").value;
 
-    let text = document.querySelector('#search').value;
+  // Get images
+  let data = await getImages(text);
 
-    // Get images
-    let data = await getImages(text);
+  // updateUI
+  updateUI(data);
+});
 
-    // updateUI
-    updateUI(data);
-})
+async function getImages(text) {
+  // const apiKey ='19d3e6e0acfe9c438f368e2c2bab1c5d';
+  const apiKey = "69fbf22c6cf92adf7e41c8560b32eec9";
+  const baseUrl = "https://api.flickr.com/services/rest/";
+  let method = "flickr.photos.search";
+  let selectValue = document.querySelector("select").value;
+  let url = `${baseUrl}?api_key=${apiKey}&per_page=${selectValue}&method=${method}&text=${text}&format=json&nojsoncallback=1`;
 
-async function getImages(text){
-
-    const apiKey ='19d3e6e0acfe9c438f368e2c2bab1c5d';
-    const baseUrl ='https://api.flickr.com/services/rest/';
-    let method = 'flickr.photos.search';
-    let selectValue = document.querySelector('select').value;
-    let url = `${baseUrl}?api_key=${apiKey}&per_page=${selectValue}&method=${method}&text=${text}&format=json&nojsoncallback=1`;
-
-    let resp = await fetch(url);
-    let data = await resp.json();
-    return data.photos;
-
+  let resp = await fetch(url);
+  let data = await resp.json();
+  return data.photos;
 }
 
 // Update UI
-function updateUI(data){
+function updateUI(data) {
+  // Clear photos
+  document.querySelector("#photos").innerHTML = "";
 
-    // Clear photos
-    document.querySelector('#photos').innerHTML = '';
+  data.photo.forEach((img) => {
+    let el = document.createElement("img");
+    el.setAttribute("src", imgUrl(img, "q"));
+    el.setAttribute("data-org", imgUrl(img, "c"));
 
-    data.photo.forEach(img => {
-        let el = document.createElement('img');
-        el.setAttribute('src', imgUrl(img, 'q'));
-        el.setAttribute('data-org', imgUrl(img, 'c'));
+    document.querySelector("#photos").appendChild(el);
+  });
 
-        document.querySelector('#photos').appendChild(el);
+  // Lightbox
 
+  const lightbox = document.querySelector("#lightbox");
+  const images = document.querySelectorAll("img");
+  images.forEach((image) => {
+    image.addEventListener("click", (e) => {
+      lightbox.classList.add("active");
+      const img = document.createElement("img");
+      img.src = image.src;
+      while (lightbox.firstChild) {
+        lightbox.removeChild(lightbox.firstChild);
+      }
+      lightbox.appendChild(img);
     });
+  });
 
-    // Lightbox
-
-const lightbox = document.querySelector('#lightbox') 
-const images = document.querySelectorAll('img') 
-images.forEach(image => { 
-image.addEventListener('click', e => {     
-        lightbox.classList.add('active')         
-        const img = document.createElement('img')     
-        img.src = image.src     
-        while (lightbox.firstChild) {     
-        lightbox.removeChild(lightbox.firstChild)     
-        }     
-        lightbox.appendChild(img)     
-    }) 
-})
-
-lightbox.addEventListener('click', e => {
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
-})
-
+  lightbox.addEventListener("click", (e) => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove("active");
+  });
 }
-
 
 // Construct img url
-function imgUrl(img, size){
-
-    return `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${size}.jpg`
-
-
+function imgUrl(img, size) {
+  return `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${size}.jpg`;
 }
-
-
-
